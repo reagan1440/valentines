8869debf196f6d8c3b616c6650f50ce5d283d365
reagan1440 <122570333+reagan1440@users.noreply.github.com>
2024-02-14 12:25:21 -0500

    code

---
+++ index.js
@@ -0,0 +1,6 @@
+  document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('valentineButton').addEventListener('click', function() {
+      alert('You are my Valentine! 💖');
+    });
+  });
+  
